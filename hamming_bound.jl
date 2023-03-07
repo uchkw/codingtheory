40ec47d0905a278d1d5fb96bc9453e92f130b532
uchkw <huchkw@gmail.com>
2023-03-07 23:31:04 +0900

    change function name in hamming_bound.jl

--- hamming_bound.jl
+++ hamming_bound.jl
@@ -3,17 +3,18 @@
 function volume(n::Int64, r::Int64)
     s = 1
     for i=1:r
-        s += binomial(n,i)
+        s += binomial(big(n),i)
     end
     return s
 end
 
 #%%
-function get_achievable_dmin(n::Int64, k::Int64)::Int64
+# Hamming bound: upper bound of minimum distance
+function get_dmin_ub(n::Int64, k::Int64)::Int64
     dmin = 1
     for d=2:n-k
         if log2(volume(n, floor(Int64, (d-1)/2))) > n-k
-            println("dmin= $dmin")
+            println("dmin_ub= $dmin")
             break
         else
             dmin = d
@@ -24,6 +25,6 @@ end
 
 #%%
 
-get_achievable_dmin(511,493)
-get_achievable_dmin(512,493)
-get_achievable_dmin(144,128)
+# get_dmin_ub(511,493)
+# get_dmin_ub(512,493)
+# get_dmin_ub(144,128)
